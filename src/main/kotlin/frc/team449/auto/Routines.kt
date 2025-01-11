022026ffd046ffdf8eecdf6457df565d88810c65
Tinbite-A <tinbite724@gmail.com>
2025-01-11 13:54:46 -0500

    added commands

--- src/main/kotlin/frc/team449/auto/Routines.kt
+++ src/main/kotlin/frc/team449/auto/Routines.kt
@@ -22,20 +22,61 @@ class Routines(
 
   fun doNothing(): Command {
     return Commands.sequence()
-
   }
 
+  fun taxi(): Command{
+    return Commands.sequence(
+      autoFactory.resetOdometry("leftTaxi"),
+      PrintCommand("resetting odometry and then moving forward"),
+      autoFactory.trajectoryCmd("leftTaxi")
+    )
+  }
+
+  fun l4reefE():Command{
+    return Commands.sequence(
+      autoFactory.resetOdometry("right_E"),
+      Commands.parallel(
+        autoFactory.trajectoryCmd("right_E"),
+        PrintCommand("elevator extending"),
+      ),
+      PrintCommand("wait for a bit to align the coral"),
+      PrintCommand("placing coral"),
+    )
+  }
+
+  fun l1reefJ():Command{
+    return Commands.sequence(
+      autoFactory.resetOdometry("left_J"),
+      autoFactory.trajectoryCmd("left_J"),
+      PrintCommand("place coral in l1")
+    )
+  }
+
+  fun l1reefG():Command{
+    return Commands.sequence(
+      autoFactory.resetOdometry("middle_G"),
+      autoFactory.trajectoryCmd("middle_G"),
+      PrintCommand("place coral in l1")
+    )
+  }
+
+  fun l1reefF():Command{
+    return Commands.sequence(
+      autoFactory.resetOdometry("left_F"),
+      autoFactory.trajectoryCmd("left_F"),
+      PrintCommand("place coral in l1"))
+  }
 
   fun Shootl4(): Command{
-     return Commands.sequence(
-       autoFactory.resetOdometry("Go to l4F"),
-       Commands.parallel(
-         autoFactory.trajectoryCmd("Go to l4F"),
-         PrintCommand("Going to l4f")
-     ),
-       PrintCommand("Shoot l4"),
+    return Commands.sequence(
+      autoFactory.resetOdometry("Go to l4F"),
+      Commands.parallel(
+        autoFactory.trajectoryCmd("Go to l4F"),
+        PrintCommand("Going to l4f")
+      ),
+      PrintCommand("Shoot l4"),
 
-    )
+      )
   }
 
   fun goToRightStation():Command{
@@ -64,7 +105,13 @@ class Routines(
 
   }
 
+
   fun addOptions(autoChooser: AutoChooser) {
     autoChooser.addCmd("Do Nothing", this::doNothing)
+    autoChooser.addCmd("Left Taxi", this::taxi)
+    autoChooser.addCmd("L4 To reef E", this::l4reefE)
+    autoChooser.addCmd("L1 To reef F", this::l1reefF)
+    autoChooser.addCmd("L1 To reef G", this::l1reefG)
+    autoChooser.addCmd("L1 To reef J", this::l1reefJ)
   }
 }
