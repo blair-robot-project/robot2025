820d3c058453186790c3117dfd22fc2097ca0bee
MadMath123 <william@n2haq.com>
2025-01-16 15:28:55 -0500

    this is a cat

---
+++ src/main/kotlin/frc/team449/subsystems/drive/new_swerve/SwerveDrive.kt
@@ -0,0 +1,26 @@
+package frc.team449.subsystems.drive.new_swerve
+
+import edu.wpi.first.math.geometry.Pose2d
+import edu.wpi.first.math.geometry.Rotation2d
+import edu.wpi.first.math.kinematics.ChassisSpeeds
+import edu.wpi.first.math.kinematics.SwerveModuleState
+import edu.wpi.first.wpilibj2.command.Subsystem
+
+
+interface SwerveDrive: Subsystem{
+  fun drive(speeds: ChassisSpeeds, fieldRelative: Boolean, isOpenLoop: Boolean)
+  fun setModuleStates(desiredStates: Array<SwerveModuleState>)
+  fun getMeasuredSpeeds()
+  fun getGyroYaw()
+  fun getPose()
+  fun setPose(pose: Pose2d)
+  fun getHeading(){
+    return getPose().getRotation();
+  }
+  fun setHeading(heading: Rotation2d){
+    setPose(Pose2d(getPose().getTranslation(), heading))
+  }
+  fun zeroHeading(){
+    setHeading(Rotation2d())
+  };
+}
