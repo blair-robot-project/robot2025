9d03c72e56a51d89635a26719ed7b389ffe61904
John Joseph <jpothen8@gmail.com>
2025-01-18 18:28:48 -0500

    Revert "added ChoreoLib2025.json to git"
    This reverts commit 9e9de78d

--- src/main/kotlin/frc/team449/subsystems/drive/DriveSubsystem.kt
+++ src/main/kotlin/frc/team449/subsystems/drive/DriveSubsystem.kt
@@ -9,15 +9,16 @@ import edu.wpi.first.math.geometry.Translation2d
 import edu.wpi.first.math.kinematics.ChassisSpeeds
 import edu.wpi.first.wpilibj2.command.Subsystem
 
+
 /** A drivetrain that uses closed-loop velocity control. */
 interface DriveSubsystem : Subsystem {
 
   private val xController: PIDController
     get() = PIDController(10.0, 0.0, 0.0)
   private val yController: PIDController
-    get() = PIDController(10.0, 0.0, 0.0)
+    get()=PIDController(10.0, 0.0, 0.0)
   private val headingController: PIDController
-    get() = PIDController(7.5, 0.0, 0.0)
+    get()= PIDController(7.5, 0.0, 0.0)
 
   var heading: Rotation2d
     get() = Rotation2d(MathUtil.angleModulus(this.pose.rotation.radians))
@@ -27,12 +28,12 @@ interface DriveSubsystem : Subsystem {
 
   var pose: Pose2d
 
-  fun getPos(): Pose2d {
-    return Pose2d(pose.x, pose.y, heading)
+  fun getPose(): Pose2d {
+    return Pose2d(pose.x , pose.y , heading)
   }
   fun followTrajectory(sample: SwerveSample) {
     // Get the current pose of the robot
-    val pose: Pose2d = getPos()
+    val pose: Pose2d = getPose();
 
     // Generate the next speeds for the robot
     val speeds: ChassisSpeeds = ChassisSpeeds(
@@ -47,15 +48,12 @@ interface DriveSubsystem : Subsystem {
 
   fun driveFieldRelative(speeds: ChassisSpeeds)
 
-  /** Sets the drivetrain's desired speeds. */
-  fun set(desiredSpeeds: ChassisSpeeds){
 
-  }
+  /** Sets the drivetrain's desired speeds. */
+  fun set(desiredSpeeds: ChassisSpeeds)
 
   /** Sets all the robot's drive motors to 0. */
-  fun stop(){
-
-  }
+  fun stop()
 
   /**
    * Used to simulate a drivetrain. Only one instance of this class should be made per drivetrain.
