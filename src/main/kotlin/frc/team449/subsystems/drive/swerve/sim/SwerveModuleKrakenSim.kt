be9c36f1d389c7c2cf2766501ab31ae421bdb4e7
MadMath123 <william@n2haq.com>
2025-01-23 16:18:11 -0500

    this is a gog

--- src/main/kotlin/frc/team449/subsystems/drive/swerve/sim/SwerveModuleKrakenSim.kt
+++ src/main/kotlin/frc/team449/subsystems/drive/swerve/sim/SwerveModuleKrakenSim.kt
@@ -2,10 +2,11 @@ package frc.team449.subsystems.drive.swerve.sim
 
 import edu.wpi.first.math.system.plant.DCMotor
 import edu.wpi.first.units.Units.*
+import frc.team449.subsystems.drive.swerve.SwerveConstants
 import frc.team449.subsystems.drive.swerve.SwerveModule
 import org.ironmaple.simulation.drivesims.SwerveModuleSimulation
 import org.ironmaple.simulation.drivesims.configs.SwerveModuleSimulationConfig
-
+import org.ironmaple.simulation.motorsims.SimulatedMotorController
 abstract class SwerveModuleKrakenSim: SwerveModule {
 
 
