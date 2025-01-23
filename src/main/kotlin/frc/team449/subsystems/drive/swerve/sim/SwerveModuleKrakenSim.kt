ed4a79894e00faf55d3c3fa6dad2856db79f83e3
Michael Spears <mts18142027@gmail.com>
2025-01-23 15:54:25 -0500

    Added Maplesim SwerveModuleSimulation to SwerveModuleKrakenSim.kt

--- src/main/kotlin/frc/team449/subsystems/drive/swerve/sim/SwerveModuleKrakenSim.kt
+++ src/main/kotlin/frc/team449/subsystems/drive/swerve/sim/SwerveModuleKrakenSim.kt
@@ -1,5 +1,23 @@
 package frc.team449.subsystems.drive.swerve.sim
 
-class SwerveModuleKrakenSim: SwerveModule{
+import edu.wpi.first.math.system.plant.DCMotor
+import edu.wpi.first.units.Units.*
+import frc.team449.subsystems.drive.swerve.SwerveModule
+import org.ironmaple.simulation.drivesims.SwerveModuleSimulation
+import org.ironmaple.simulation.drivesims.configs.SwerveModuleSimulationConfig
 
+abstract class SwerveModuleKrakenSim: SwerveModule {
+  val module: SwerveModuleSimulation = SwerveModuleSimulation(
+    SwerveModuleSimulationConfig(
+      DCMotor.getKrakenX60(1),
+      DCMotor.getKrakenX60(1),
+      0.0,
+      0.0,
+      Volts.of(12.0),
+      Volts.of(12.0),
+      Meters.of(1.1),
+      KilogramSquareMeters.of(12.0),
+      0.0
+    )
+  )
 }
