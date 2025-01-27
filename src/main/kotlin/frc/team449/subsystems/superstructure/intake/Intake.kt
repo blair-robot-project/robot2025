07d7b2e379a075cd035f4c93db466e3fcff7c0f4
John Joseph <jpothen8@gmail.com>
2025-01-26 22:00:05 -0500

    add IR sensor to intake

--- src/main/kotlin/frc/team449/subsystems/superstructure/intake/Intake.kt
+++ src/main/kotlin/frc/team449/subsystems/superstructure/intake/Intake.kt
@@ -1,15 +1,16 @@
 package frc.team449.subsystems.superstructure.intake
 
-import com.ctre.phoenix6.hardware.TalonFX
-import edu.wpi.first.units.Units.Seconds
+import com.revrobotics.spark.SparkMax
+import edu.wpi.first.wpilibj.DigitalInput
 import edu.wpi.first.wpilibj2.command.Command
 import edu.wpi.first.wpilibj2.command.SubsystemBase
-import frc.team449.system.motor.createKraken
+import frc.team449.system.motor.createSparkMax
 
 // TODO(the entire class bru)
 class Intake(
-  private val frontMotor: TalonFX,
-  private val backMotor: TalonFX
+  private val frontMotor: SparkMax,
+  private val backMotor: SparkMax,
+  val infrared: DigitalInput
 ) : SubsystemBase() {
 
   fun intakeCoral(): Command {
@@ -49,24 +50,23 @@ class Intake(
 
   companion object {
     fun createIntake(): Intake {
-      val frontMotor = createKraken(
+      val frontMotor = createSparkMax(
         id = IntakeConstants.FRONT_MOTOR_ID,
         inverted = IntakeConstants.FRONT_MOTOR_INVERTED,
-        statorCurrentLimit = IntakeConstants.STATOR_LIMIT,
-        burstCurrentLimit = IntakeConstants.SUPPLY_LIMIT,
-        burstTimeLimit = Seconds.of(0.0),
-        updateFrequency = IntakeConstants.VALUE_UPDATE_FREQ
+        brakeMode = IntakeConstants.BRAKE_MODE,
+        currentLimit = IntakeConstants.CURRENT_LIMIT
       )
 
-      val backMotor = createKraken(
+      val backMotor = createSparkMax(
         id = IntakeConstants.BACK_MOTOR_ID,
         inverted = IntakeConstants.BACK_MOTOR_INVERTED,
-        statorCurrentLimit = IntakeConstants.STATOR_LIMIT,
-        burstCurrentLimit = IntakeConstants.SUPPLY_LIMIT,
-        burstTimeLimit = Seconds.of(0.0),
-        updateFrequency = IntakeConstants.VALUE_UPDATE_FREQ
+        brakeMode = IntakeConstants.BRAKE_MODE,
+        currentLimit = IntakeConstants.CURRENT_LIMIT
       )
-      return Intake(frontMotor, backMotor)
+
+      val sensor = DigitalInput(IntakeConstants.SENSOR_DIO_PORT)
+
+      return Intake(frontMotor, backMotor, sensor)
     }
   }
 }
--- src/main/kotlin/frc/team449/subsystems/superstructure/intake/IntakeConstants.kt
+++ src/main/kotlin/frc/team449/subsystems/superstructure/intake/IntakeConstants.kt
@@ -1,16 +1,13 @@
 package frc.team449.subsystems.superstructure.intake
 
 import edu.wpi.first.units.Units.Amps
-import edu.wpi.first.units.Units.Hertz
 
 object IntakeConstants {
   const val FRONT_MOTOR_ID = 10 // TODO(Change motor ID.)
   const val BACK_MOTOR_ID = 11 // TODO(Change motor ID.)
 
-  val STATOR_LIMIT = Amps.of(60.0)
-  val SUPPLY_LIMIT = Amps.of(30.0)
-
-  val VALUE_UPDATE_FREQ = Hertz.of(10.0)
+  val CURRENT_LIMIT = Amps.of(30.0)
+  const val BRAKE_MODE = true
 
   const val FRONT_MOTOR_INVERTED = false
   const val BACK_MOTOR_INVERTED = true
@@ -19,4 +16,6 @@ object IntakeConstants {
   const val CORAL_OUTTAKE_VOLTAGE = -2.0
   const val ALGAE_INTAKE_VOLTAGE = 7.0
   const val ALGAE_OUTTAKE_VOLTAGE = -10.0
+
+  const val SENSOR_DIO_PORT = 11
 }
