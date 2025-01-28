4b9f916848890506db40add0bde97121202e4686
EdzmLeGoat <edemsetse@gmail.com>
2025-01-28 15:50:26 -0500

    a tiny tiny tiny change that i made cuz i felt like it

--- src/main/kotlin/frc/team449/commands/autoscoreCommands/AutoScoreCommands.kt
+++ src/main/kotlin/frc/team449/commands/autoscoreCommands/AutoScoreCommands.kt
@@ -32,7 +32,7 @@ class AutoScoreCommands(
   //we'll use magnetize if not
   val usingPathfinding = true
   init {
-    if (usingPathfinding) println("                                      pathfinding") else println("                                                         magnetize")
+    if (usingPathfinding) println("pathfinding") else println("magnezite")
   }
 
   /**
