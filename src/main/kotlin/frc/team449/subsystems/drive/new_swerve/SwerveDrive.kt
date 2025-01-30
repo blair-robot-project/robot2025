9be1696f5180857c16cbed350f1a7b296b6b6bcf
Michael Spears <mts18142027@gmail.com>
2025-01-30 16:18:56 -0500

    Fixed errors in new_swerve/SwerveDrive.kt

--- src/main/kotlin/frc/team449/subsystems/drive/new_swerve/SwerveDrive.kt
+++ src/main/kotlin/frc/team449/subsystems/drive/new_swerve/SwerveDrive.kt
@@ -1,9 +1,12 @@
 package frc.team449.subsystems.drive.new_swerve
 
+import edu.wpi.first.math.Matrix
 import edu.wpi.first.math.geometry.Pose2d
 import edu.wpi.first.math.geometry.Rotation2d
 import edu.wpi.first.math.kinematics.ChassisSpeeds
 import edu.wpi.first.math.kinematics.SwerveModuleState
+import edu.wpi.first.math.numbers.N1
+import edu.wpi.first.math.numbers.N3
 import edu.wpi.first.wpilibj2.command.Subsystem
 
 
@@ -14,17 +17,17 @@ interface SwerveDrive: Subsystem{
   fun getGyroYaw(): Rotation2d
   fun getPose(): Pose2d
   fun setPose(pose: Pose2d)
-  fun getHeading(){
-    return getPose().getRotation();
+  fun getHeading(): Rotation2d {
+    return getPose().rotation
   }
   fun setHeading(heading: Rotation2d){
-    setPose(Pose2d(getPose().getTranslation(), heading))
+    setPose(Pose2d(getPose().translation, heading))
   }
   fun zeroHeading(){
     setHeading(Rotation2d())
   }
-  fun addVisionMeasurement(visionRobotPose: Pose2d, timeStampSeconds: float){}
-  fun addVisionMeasurement(visionRobotPoseMeters: Pose2d, timeStampSeconds: float, visionMeasurementStdDevs: Matrix<N3, N1>){
+  fun addVisionMeasurement(visionRobotPose: Pose2d, timeStampSeconds: Float){}
+  fun addVisionMeasurement(visionRobotPoseMeters: Pose2d, timeStampSeconds: Float, visionMeasurementStdDevs: Matrix<N3, N1>){
 
   }
 }
