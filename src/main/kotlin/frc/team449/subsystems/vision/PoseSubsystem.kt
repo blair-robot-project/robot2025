44dea990d68b1d20dad18f930fb1331e6c91de63
Edem Setse <edemsetse@gmail.com>
2025-02-05 21:45:40 -0500

    ok NOW we're all good, monologue is removed and now I can add in the scoring from the superstructure

--- src/main/kotlin/frc/team449/subsystems/vision/PoseSubsystem.kt
+++ src/main/kotlin/frc/team449/subsystems/vision/PoseSubsystem.kt
@@ -1,7 +1,6 @@
 package frc.team449.subsystems.vision
 
-import dev.doglog.DogLog
-import edu.wpi.first.epilogue.Logged
+
 import edu.wpi.first.math.MathUtil
 import edu.wpi.first.math.controller.PIDController
 import edu.wpi.first.math.estimator.SwerveDrivePoseEstimator
@@ -31,7 +30,7 @@ import frc.team449.system.AHRS
 import kotlin.jvm.optionals.getOrNull
 import kotlin.math.*
 
-@Logged
+
 class PoseSubsystem(
   private val ahrs: AHRS,
   private val cameras: List<ApriltagCamera> = mutableListOf(),
@@ -46,11 +45,14 @@ class PoseSubsystem(
   private val fieldOriented: () -> Boolean = { true },
 ) : SubsystemBase() {
 
+
   /** magnetize stuff */
   private var prevX = 0.0
   private var prevY = 0.0
 
+
   private var prevTime = 0.0
+
 
   private var dx = 0.0
   private var dy = 0.0
@@ -58,17 +60,22 @@ class PoseSubsystem(
   private var dt = 0.0
   private var magAccClamped = 0.0
 
+
   private var rotScaled = 0.0
   private val allianceCompensation = { if (DriverStation.getAlliance().getOrNull() == DriverStation.Alliance.Red) PI else 0.0 }
   private val directionCompensation = { if (DriverStation.getAlliance().getOrNull() == DriverStation.Alliance.Red) -1.0 else 1.0 }
 
+
   var headingLock = false
+
 
   private var rotRamp = SlewRateLimiter(RobotConstants.ROT_RATE_LIMIT)
 
+
   private val timer = Timer()
-//  lateinit var endPose: Pose2d
+  //  lateinit var endPose: Pose2d
   var autoscoreCommandPose = Pose2d(0.0, 0.0,Rotation2d(0.0))
+
 
   private val rotCtrl = PIDController(
     RobotConstants.SNAP_KP,
@@ -76,19 +83,24 @@ class PoseSubsystem(
     RobotConstants.SNAP_KD
   )
 
+
   private var skewConstant = SwerveConstants.SKEW_CONSTANT
   private var desiredVel = doubleArrayOf(0.0, 0.0, 0.0)
 
+
   //edem mag vars
   private var currentControllerPower = 15.0
-  private var controllerMult = 1.00
+  private var magMultiply = 1.00
   private val magIncConstant = 0.001
-  private var magDecConstant = 0.0004
+  private var magDec = 0.0004
   private val maxMagPower = 20.0
-  //placeholder extremely big number
   private var lastDistance = 0.0
   val autoDistance = 0.5
   lateinit var autoscoreCurrentCommand : Command
+
+
+
+
 
 
 
@@ -97,14 +109,18 @@ class PoseSubsystem(
     xController.reset()
     xController.setTolerance(poseTol.x)
 
+
     yController.reset()
     yController.setTolerance(poseTol.y)
+
 
     thetaController.reset()
     thetaController.enableContinuousInput(-PI, PI)
     thetaController.setTolerance(poseTol.rotation.radians)
 
+
     timer.restart()
+
 
     prevX = drive.currentSpeeds.vxMetersPerSecond
     prevY = drive.currentSpeeds.vyMetersPerSecond
@@ -115,83 +131,112 @@ class PoseSubsystem(
     dt = 0.0
     magAccClamped = 0.0
 
+
     rotRamp = SlewRateLimiter(
       RobotConstants.ROT_RATE_LIMIT,
       RobotConstants.NEG_ROT_RATE_LIM,
       drive.currentSpeeds.omegaRadiansPerSecond
     )
 
+
     headingLock = false
+
 
     xController.reset()
     yController.reset()
     thetaController.reset()
 
+
     timer.restart()
+
 
     resetMagVars()
   }
+
 
   private fun calculate(currPose: Pose2d, desState: Pose2d): ChassisSpeeds {
     val xPID = xController.calculate(currPose.x, desState.x)
     val yPID = yController.calculate(currPose.y, desState.y)
     val angPID = thetaController.calculate(currPose.rotation.radians, desState.rotation.radians)
 
+
     return ChassisSpeeds.fromFieldRelativeSpeeds(xPID, yPID, angPID, currPose.rotation)
   }
+
 
   private fun allControllersAtReference(): Boolean {
     return xController.atSetpoint() && yController.atSetpoint() && thetaController.atSetpoint()
   }
 
-  fun resetMagVars() {
+
+  private fun resetMagVars() {
     currentControllerPower = 15.0
-    controllerMult = 1.00
-    magDecConstant = 0.0004
+    magMultiply = 1.00
+    magDec = 0.0004
     lastDistance = 0.0
   }
+
+
+  private fun clampCP() {
+    currentControllerPower = MathUtil.clamp(currentControllerPower, 0.1, maxMagPower)
+  }
+
+
+  private fun clampMult() {
+    magMultiply = MathUtil.clamp(magMultiply, 0.1, 2.0)
+  }
+
 
   fun edemPathMag(desVel: ChassisSpeeds) {
     val currTime = timer.get()
     dt = currTime - prevTime
     prevTime = currTime
+    if(dt > 0.3) {
+      resetMagVars()
+    }
     val distance = pose.translation.getDistance(autoscoreCommandPose.translation)
     val ctrlX = -controller.leftY
     val ctrlY = -controller.leftX
-    val controllerMag = sqrt(ctrlX.pow(2) + ctrlY.pow(2))
+    val controllerMag = hypot(ctrlX, ctrlY)
+
 
     if(distance > lastDistance) {
-      controllerMult += magIncConstant
+      magMultiply += magIncConstant
+      magDec -= 0.001
     } else if(distance < lastDistance) {
-      controllerMult -= magDecConstant
-      magDecConstant += 0.01
+      magMultiply -= magDec
+      magDec += 0.001
     }
+    magDec = MathUtil.clamp(magDec, 0.0, 0.5)
+    clampMult()
+    currentControllerPower *= magMultiply
 
-    controllerMult = MathUtil.clamp(controllerMult, 0.5, 2.0)
-
-    currentControllerPower *= controllerMult
 
     if(controllerMag > 0.75) {
       currentControllerPower = MathUtil.clamp(currentControllerPower, 15.0, maxMagPower)
-      controllerMult += 0.1
+      magMultiply += 0.3
     }
 
+
     //this increases the users power if they are moving a lot
+    //also decreases the users power if they are not moving a lot
     currentControllerPower += 1.2/( 1 + exp(-6 * (controllerMag-0.7) ) ) - 0.5
 
-    currentControllerPower = MathUtil.clamp(currentControllerPower, 1.0, maxMagPower)
 
+    clampCP()
     if(distance <= autoDistance || controllerMag < 0.1 || currentControllerPower < 3) {
       if(distance <= autoDistance) {
         resetMagVars()
       } else {
         currentControllerPower -= 0.1
-        controllerMult -= 0.05
+        magMultiply -= 0.05
       }
       drive.set(desVel)
     } else {
 
+
       // controller stuff
+
 
       val ctrlRadius = MathUtil.applyDeadband(
         min(sqrt(ctrlX.pow(2) + ctrlY.pow(2)), 1.0),
@@ -199,13 +244,17 @@ class PoseSubsystem(
         1.0
       ).pow(SwerveConstants.JOYSTICK_FILTER_ORDER)
 
+
       val ctrlTheta = atan2(ctrlY, ctrlX)
+
 
       val xScaled = ctrlRadius * cos(ctrlTheta) * drive.maxLinearSpeed
       val yScaled = ctrlRadius * sin(ctrlTheta) * drive.maxLinearSpeed
 
+
       var xClamped = xScaled
       var yClamped = yScaled
+
 
       if (RobotConstants.USE_ACCEL_LIMIT) {
         dx = xScaled - prevX
@@ -213,12 +262,14 @@ class PoseSubsystem(
         magAcc = hypot(dx / dt, dy / dt)
         magAccClamped = MathUtil.clamp(magAcc, -drive.accel, drive.accel)
 
+
         val factor = if (magAcc == 0.0) 0.0 else magAccClamped / magAcc
         val dxClamped = dx * factor
         val dyClamped = dy * factor
         xClamped = prevX + dxClamped
         yClamped = prevY + dyClamped
       }
+
 
       rotScaled = if (!headingLock) {
         rotRamp.calculate(
@@ -239,9 +290,12 @@ class PoseSubsystem(
         )
       }
 
+
       val vel = Translation2d(xClamped, yClamped)
 
+
       vel.rotateBy(Rotation2d(-rotScaled * dt * skewConstant))
+
 
       var controllerSpeeds = ChassisSpeeds.fromFieldRelativeSpeeds(
         vel.x * directionCompensation.invoke(),
@@ -256,23 +310,35 @@ class PoseSubsystem(
         currentControllerPower += (abs(controllerSpeeds.vyMetersPerSecond) + abs(desVel.vyMetersPerSecond))/20
       }
 
-      controllerSpeeds *= currentControllerPower
-      val desVelAdjustedSpeeds = desVel / (16 / ( 1 + exp(-(currentControllerPower-15)/2) ) )
 
-      val combinedChassisSpeeds = controllerSpeeds + desVelAdjustedSpeeds
+      controllerSpeeds *= currentControllerPower / 2
+      val desVelAdjustedSpeeds = desVel / (20 / ( 1 + exp(-(currentControllerPower-15)/2) ) )
+
+
+      val combinedChassisSpeeds = if(controllerMag > 0.95 || currentControllerPower > 16) {
+        controllerSpeeds
+      } else {
+        controllerSpeeds + desVelAdjustedSpeeds
+      }
       combinedChassisSpeeds.vxMetersPerSecond = MathUtil.clamp(combinedChassisSpeeds.vxMetersPerSecond , -drive.maxLinearSpeed, drive.maxLinearSpeed)
       combinedChassisSpeeds.vyMetersPerSecond = MathUtil.clamp(combinedChassisSpeeds.vyMetersPerSecond , -drive.maxLinearSpeed, drive.maxLinearSpeed)
       combinedChassisSpeeds.omegaRadiansPerSecond = MathUtil.clamp(combinedChassisSpeeds.omegaRadiansPerSecond, -drive.maxRotSpeed, drive.maxRotSpeed)
 
+
       drive.set(combinedChassisSpeeds)
+
 
     }
 
-    println(controllerMult)
+
+//    println("controller power: $currentControllerPower mult: $magMultiply")
+    println("mag dec: $magDec")
     lastDistance = distance
   }
 
+
   private val isReal = RobotBase.isReal()
+
 
   private val poseEstimator = SwerveDrivePoseEstimator(
     drive.kinematics,
@@ -283,11 +349,13 @@ class PoseSubsystem(
     VisionConstants.MULTI_TAG_TRUST
   )
 
+
   var heading: Rotation2d
     get() = Rotation2d(MathUtil.angleModulus(this.pose.rotation.radians))
     set(value) {
       this.pose = Pose2d(Translation2d(this.pose.x, this.pose.y), value)
     }
+
 
   /** Vision statistics */
   private val numTargets = DoubleArray(cameras.size)
@@ -298,20 +366,26 @@ class PoseSubsystem(
   private val usedVisionSights = LongArray(cameras.size)
   private val rejectedVisionSights = LongArray(cameras.size)
 
+
   var enableVisionFusion = true
+
 
   /** Current estimated vision pose */
   var visionPose = DoubleArray(cameras.size * 3)
+
 
   /** The measured pitch of the robot from the gyro sensor. */
   val pitch: Rotation2d
     get() = Rotation2d(MathUtil.angleModulus(ahrs.pitch.radians))
 
+
   /** The measured roll of the robot from the gyro sensor. */
   val roll: Rotation2d
     get() = Rotation2d(MathUtil.angleModulus(ahrs.roll.radians))
 
+
   var oldPose: Pose2d = Pose2d()
+
 
   /** The (x, y, theta) position of the robot on the field. */
   var pose: Pose2d
@@ -324,7 +398,9 @@ class PoseSubsystem(
       )
     }
 
+
   var pureVisionPose: Pose2d = Pose2d()
+
 
   init {
     SmartDashboard.putData("Elastic Swerve Drive") { builder: SendableBuilder ->
@@ -332,22 +408,28 @@ class PoseSubsystem(
       builder.addDoubleProperty("Front Left Angle", { drive.modules[0].state.angle.radians }, null)
       builder.addDoubleProperty("Front Left Velocity", { drive.modules[0].state.speedMetersPerSecond }, null)
 
+
       builder.addDoubleProperty("Front Right Angle", { drive.modules[1].state.angle.radians }, null)
       builder.addDoubleProperty("Front Right Velocity", { drive.modules[1].state.speedMetersPerSecond }, null)
+
 
       builder.addDoubleProperty("Back Left Angle", { drive.modules[2].state.angle.radians }, null)
       builder.addDoubleProperty("Back Left Velocity", { drive.modules[2].state.speedMetersPerSecond }, null)
 
+
       builder.addDoubleProperty("Back Right Angle", { drive.modules[3].state.angle.radians }, null)
       builder.addDoubleProperty("Back Right Velocity", { drive.modules[3].state.speedMetersPerSecond }, null)
+
 
       builder.addDoubleProperty("Robot Angle", { poseEstimator.estimatedPosition.rotation.radians }, null)
     }
   }
 
+
   fun resetOdometry(newPose: Pose2d) {
     this.poseEstimator.resetPose(newPose)
   }
+
 
   /**
    * pathfinds to a pose while allowing driver control through magnetization
@@ -355,16 +437,20 @@ class PoseSubsystem(
    */
   fun setMagnetizePathplanning(desState: Pose2d) {
 
+
     val xController = PIDController(AutoConstants.DEFAULT_X_KP, 0.0, 0.0)
     val yController = PIDController(AutoConstants.DEFAULT_Y_KP, 0.0, 0.0)
     val thetaController = PIDController(AutoConstants.DEFAULT_ROTATION_KP, 0.0, 0.0)
+
 
     val xPID = xController.calculate(pose.x, desState.x)
     val yPID = yController.calculate(pose.y, desState.y)
     val angPID = thetaController.calculate(pose.rotation.radians, desState.rotation.radians)
 
+
     ChassisSpeeds.fromFieldRelativeSpeeds(xPID, yPID, angPID, desState.rotation)
   }
+
 
   override fun periodic() {
     oldPose = pose
@@ -381,12 +467,13 @@ class PoseSubsystem(
       )
     }
 
+
     if (cameras.isNotEmpty()) localize()
 
-    setRobotPose()
 
-    logData()
+    setRobotPose()
   }
+
 
   private fun localize() = try {
     for ((index, camera) in cameras.withIndex()) {
@@ -398,6 +485,7 @@ class PoseSubsystem(
           tagDistance[index] = 0.0
           avgAmbiguity[index] = 0.0
           heightError[index] = abs(presentResult.estimatedPose.z)
+
 
           for (tag in presentResult.targetsUsed) {
             val tagPose = camera.estimator.fieldTags.getTagPose(tag.fiducialId)
@@ -412,16 +500,20 @@ class PoseSubsystem(
             }
           }
 
+
           val estVisionPose = presentResult.estimatedPose.toPose2d()
+
 
           visionPose[0 + 3 * index] = estVisionPose.x
           visionPose[1 + 3 * index] = estVisionPose.y
           visionPose[2 + 3 * index] = estVisionPose.rotation.radians
 
+
           val inAmbiguityTolerance = avgAmbiguity[index] <= VisionConstants.MAX_AMBIGUITY
           val inDistanceTolerance = (numTargets[index] < 2 && tagDistance[index] <= VisionConstants.MAX_DISTANCE_SINGLE_TAG) ||
             (numTargets[index] >= 2 && tagDistance[index] <= VisionConstants.MAX_DISTANCE_MULTI_TAG + (numTargets[index] - 2) * VisionConstants.NUM_TAG_FACTOR)
           val inHeightTolerance = heightError[index] < VisionConstants.MAX_HEIGHT_ERR_METERS
+
 
           if (presentResult.timestampSeconds > 0 &&
             inGyroTolerance(estVisionPose.rotation) &&
@@ -431,6 +523,7 @@ class PoseSubsystem(
           ) {
             if (enableVisionFusion) {
               val interpolatedPose = InterpolatedVision.interpolatePose(estVisionPose, index)
+
 
               poseEstimator.addVisionMeasurement(
                 interpolatedPose,
@@ -454,6 +547,7 @@ class PoseSubsystem(
     )
   }
 
+
   private fun inGyroTolerance(visionPoseRot: Rotation2d): Boolean {
     val currHeadingRad = if (isReal) {
       ahrs.heading.radians
@@ -461,6 +555,7 @@ class PoseSubsystem(
       drive as SwerveSim
       drive.currHeading.radians
     }
+
 
     val result = abs(
       MathUtil.angleModulus(
@@ -476,15 +571,19 @@ class PoseSubsystem(
       ) - 2 * PI
     ) < VisionConstants.TAG_HEADING_MAX_DEV_RAD
 
+
     return result
   }
+
 
   fun getPosea(): Pose2d {
     return pose
   }
 
+
   private fun setRobotPose() {
     this.field.robotPose = this.pose
+
 
     this.field.getObject("FL").pose = this.pose.plus(
       Transform2d(
@@ -493,6 +592,7 @@ class PoseSubsystem(
       )
     )
 
+
     this.field.getObject("FR").pose = this.pose.plus(
       Transform2d(
         drive.modules[1].location,
@@ -500,12 +600,14 @@ class PoseSubsystem(
       )
     )
 
+
     this.field.getObject("BL").pose = this.pose.plus(
       Transform2d(
         drive.modules[2].location,
         drive.getPositions()[2].angle
       )
     )
+
 
     this.field.getObject("BR").pose = this.pose.plus(
       Transform2d(
@@ -515,28 +617,35 @@ class PoseSubsystem(
     )
   }
 
-  private fun logData() {
-    DogLog.log("PoseSubsystem/Estimated Pose", pose)
 
-    DogLog.log("PoseSubsystem/Vision Stats/Used Last Vision Estimate", usedVision)
-    DogLog.log("PoseSubsystem/Vision Stats/Number of Targets", numTargets)
-    DogLog.log("PoseSubsystem/Vision Stats/Avg Tag Distance", tagDistance)
-    DogLog.log("PoseSubsystem/Vision Stats/Average Ambiguity", avgAmbiguity)
-    DogLog.log("PoseSubsystem/Vision Stats/Cam Height Error", heightError)
-    DogLog.log("PoseSubsystem/Vision Stats/Total Used Vision Sights", usedVisionSights)
-    DogLog.log("PoseSubsystem/Vision Stats/Total Rejected Vision Sights", rejectedVisionSights)
+  override fun initSendable(builder: SendableBuilder) {
+    builder.publishConstString("1.0", "Pose")
+    builder.addDoubleArrayProperty("1.1 Estimated Pose", { doubleArrayOf(pose.x, pose.y, pose.rotation.radians) }, null)
+
+
+    builder.publishConstString("2.0", "Vision Stats")
+    builder.addBooleanArrayProperty("2.1 Used Last Vision Estimate?", { usedVision }, null)
+    builder.addDoubleArrayProperty("2.2 Number of Targets", { numTargets }, null)
+    builder.addDoubleArrayProperty("2.3 Avg Tag Distance", { tagDistance }, null)
+    builder.addDoubleArrayProperty("2.4 Average Ambiguity", { avgAmbiguity }, null)
+    builder.addDoubleArrayProperty("2.5 Cam Height Error", { heightError }, null)
+    builder.addIntegerArrayProperty("2.6 Total Used Vision Sights", { usedVisionSights }, null)
+    builder.addIntegerArrayProperty("2.7 Total Rejected Vision Sights", { rejectedVisionSights }, null)
     for ((index, _) in cameras.withIndex()) {
-      DogLog.log("PoseSubsystem/Vision Stats/Vision Pose Cam $index", visionPose.slice(IntRange(0 + 3 * index, 2 + 3 * index)).toDoubleArray())
+      builder.addDoubleArrayProperty("2.8${1 + index} Vision Pose Cam $index", { visionPose.slice(IntRange(0 + 3 * index, 2 + 3 * index)).toDoubleArray() }, null)
     }
-    DogLog.log("PoseSubsystem/Vision Stats/Enabled Vision Fusion", enableVisionFusion)
+    builder.addBooleanProperty("2.9 Enabled Vision Fusion", { enableVisionFusion }, null)
 
-    DogLog.log("PoseSubsystem/AHRS Values/Heading Degrees", ahrs.heading.degrees)
-    DogLog.log("PoseSubsystem/AHRS Values/Pitch Degrees", ahrs.pitch.degrees)
-    DogLog.log("PoseSubsystem/AHRS Values/Roll Degrees", ahrs.roll.degrees)
-    DogLog.log("PoseSubsystem/AHRS Values/Angular X Vel", ahrs.angularXVel())
-    DogLog.log("PoseSubsystem/AHRS Values/Navx Connected", ahrs.connected())
-    DogLog.log("PoseSubsystem/AHRS Values/Navx Calibrated", ahrs.calibrated())
+
+    builder.publishConstString("3.0", "AHRS Values")
+    builder.addDoubleProperty("3.1 Heading Degrees", { ahrs.heading.degrees }, null)
+    builder.addDoubleProperty("3.2 Pitch Degrees", { ahrs.pitch.degrees }, null)
+    builder.addDoubleProperty("3.3 Roll Degrees", { ahrs.roll.degrees }, null)
+    builder.addDoubleProperty("3.4 Angular X Vel", { ahrs.angularXVel() }, null)
+    builder.addBooleanProperty("3.5 Navx Connected", { ahrs.connected() }, null)
+    builder.addBooleanProperty("3.6 Navx Calibrated", { ahrs.calibrated() }, null)
   }
+
 
   companion object {
     fun createPoseSubsystem(ahrs: AHRS, drive: SwerveDrive, field: Field2d, controller: CommandXboxController): PoseSubsystem {
@@ -550,3 +659,4 @@ class PoseSubsystem(
     }
   }
 }
+
